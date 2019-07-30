c2280b6e202a436747f846c818aa52c23f123481
Pushpinder  Singh <pushpinder_19singh@yahoo.co.in>
2019-07-30 17:46:08 +0530

    Update edituser.component.ts
    Edit User Feature

--- src/app/auth/edituser/edituser.component.ts
+++ src/app/auth/edituser/edituser.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FeatureState, EditUser, featureQuery } from '../+state';
+import { Store } from '@ngrx/store';
 
 @Component({
-  selector: 'app-edituser',
-  templateUrl: './edituser.component.html',
-  styleUrls: ['./edituser.component.css']
+  selector: 'edit-user',
+  templateUrl: './edit-user.component.html',
+  styleUrls: ['./edit-user.component.css']
 })
-export class EdituserComponent implements OnInit {
+export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  editForm: FormGroup
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store<FeatureState>
+  ) {
+    this.initForm();
   }
 
+  ngOnInit() {
+    this.store.select(featureQuery.setSelectedUser).subscribe(user => {
+      this.editForm.patchValue({
+        enroll_id: user.enroll_id,
+        username: user.username,
+        password: user.password,
+        address: user.address,
+      });
+    });
+  }
+
+  initForm() {
+    this.editForm = this.formBuilder.group({
+      'enroll_id': ['', Validators.required],
+      'username': ['', Validators.required],
+      'password': ['', Validators.required],
+      'address': ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    // if(this.editForm.invalid) {
+    //   return;
+    // }
+    // console.log("Form Data : ", this.editForm.value);
+    this.store.dispatch(new EditUser(this.editForm.value));
+
+    this.router.navigate(['../users'], {
+      relativeTo: this.route
+    })
+  }
 }
